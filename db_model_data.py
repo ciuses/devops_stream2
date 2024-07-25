118d8f9b05d19bef244d226d2e73cb62db32e3bd
ciuse <ciuse@yandex.ru>
2024-07-25 15:01:54 +0700

    переписал ORM как в тьюториале от версии 2.0.31

--- db_model_data.py
+++ db_model_data.py
@@ -1,6 +1,7 @@
 import os
 import sqlalchemy as alch
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
 from dotenv import load_dotenv
 
 
@@ -9,30 +10,31 @@ def create_base(dsn='DSN', b_name='base_name'):
         con.execute(alch.sql.text(f'CREATE DATABASE {b_name}'))
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 class Telephons(Base):
     __tablename__ = 't_numbers'
-    id = alch.Column(alch.Integer, primary_key=True)
-    number = alch.Column(alch.VARCHAR(length=20), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    number: Mapped[str] = mapped_column(String(20), nullable=False)
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return f'id={self.id}\nnumber={self.number}'
 
 
 class Emails(Base):
     __tablename__ = 'emails'
-    id = alch.Column(alch.Integer, primary_key=True)
-    email = alch.Column(alch.String(length=160), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(160), nullable=False)
 
-    def __str__(self):
+    def __repr__(self) -> str:
         return f'id={self.id}\nemail={self.email}'
 
 def create_tables(dsn='DSN'):
     engine = alch.create_engine(dsn)
     Base.metadata.drop_all(engine) # можно не бропать базки
     Base.metadata.create_all(engine)
-    return sessionmaker(bind=engine)
+    return Session(engine)
 
 def fill_the_base(session):
     tel1 = Telephons(number='+79039533773')
@@ -41,16 +43,16 @@ def fill_the_base(session):
 
     email1 = Emails(id=1, email='ciuse@yandex.ru')
     email2 = Emails(id=2, email='ciuse@mail.ru')
-    email3 = Emails(id=3, email='zzzzz@zzz.ru')
+    email3 = Emails(id=3, email='zzzzz@zzyyyyyyz.ru')
 
     my_data = [tel1, tel2, tel3, email1, email2, email3]
 
-    my_session = session()
-    for dat in my_data:
-        my_session.add(dat)
+    # for dat in my_data:
+    #     my_session.add(dat)
 
-    my_session.commit()
-    my_session.close()
+    session.add_all(my_data)
+    session.commit()
+    session.close()
 
 
 if __name__ == '__main__':
@@ -61,7 +63,7 @@ if __name__ == '__main__':
              f"{os.getenv('db_h')}:{os.getenv('db_po')}/{data_base_name}")
 
     #создать базку
-    # create_base(dsn=DSN, b_name=data_base_name)
+    create_base(dsn=DSN, b_name=data_base_name)
     #создать таблички
     ses = create_tables(dsn=DNS_2)
     #налить данных
