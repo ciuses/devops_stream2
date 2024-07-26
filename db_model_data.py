b186ce5609e075521c2a75639df047c30b21d5d0
ciuse <ciuse@yandex.ru>
2024-07-26 16:14:24 +0700

    добавил автоинкремент для айдишников и написал точечную функцию вставки

--- db_model_data.py
+++ db_model_data.py
@@ -15,16 +15,16 @@ class Base(DeclarativeBase):
 
 class Telephons(Base):
     __tablename__ = 't_numbers'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     number: Mapped[str] = mapped_column(String(20), nullable=False)
 
     def __repr__(self) -> str:
         return f'id={self.id}\nnumber={self.number}'
 
 
-class Emails(Base):
+class Emails(Base): # todo переписать email на address
     __tablename__ = 'emails'
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     email: Mapped[str] = mapped_column(String(160), nullable=False)
 
     def __repr__(self) -> str:
@@ -32,18 +32,18 @@ class Emails(Base):
 
 def create_tables(dsn='DSN'):
     engine = alch.create_engine(dsn)
-    Base.metadata.drop_all(engine) # можно не бропать базки
+    Base.metadata.drop_all(engine) # можно не дропать базки
     Base.metadata.create_all(engine)
     return Session(engine)
 
 def fill_the_base(session):
-    tel1 = Telephons(number='+79039533773')
+    tel1 = Telephons(number='+79039533663')
     tel2 = Telephons(number='+79999999999')
     tel3 = Telephons(number='+70000000000')
 
-    email1 = Emails(id=1, email='ciuse@yandex.ru')
-    email2 = Emails(id=2, email='ciuse@mail.ru')
-    email3 = Emails(id=3, email='zzzzz@zzyyyyyyz.ru')
+    email1 = Emails(email='ddfbfdb@yandex.ru')
+    email2 = Emails(email='dbfdbdfb@mail.ru')
+    email3 = Emails(email='zzzzz@zzyyyyyyz.ru')
 
     my_data = [tel1, tel2, tel3, email1, email2, email3]
 
@@ -54,6 +54,17 @@ def fill_the_base(session):
     session.commit()
     session.close()
 
+def add_the_data(dsn='DSN'):
+    my_se = Session(alch.create_engine(dsn))
+    any_tel = Telephons(number='+73333333333')
+    my_se.add(any_tel)
+    any_emeil = Emails(email='fhbdbdbfdda@ipyp.com')
+    my_se.add(any_emeil)
+
+    my_se.commit()
+    my_se.close()
+
+
 
 if __name__ == '__main__':
     load_dotenv()
@@ -63,9 +74,12 @@ if __name__ == '__main__':
              f"{os.getenv('db_h')}:{os.getenv('db_po')}/{data_base_name}")
 
     #создать базку
-    create_base(dsn=DSN, b_name=data_base_name)
+    # create_base(dsn=DSN, b_name=data_base_name)
     #создать таблички
     ses = create_tables(dsn=DNS_2)
     #налить данных
     fill_the_base(ses)
+
+    add_the_data(dsn=DNS_2)
+
 
