7d82ffe0d14b9ba887c1c15e2d5d4783f02bcff9
ciuse <ciuse@yandex.ru>
2024-08-26 19:11:03 +0700

    ката на хвост поменял

--- linux_funcs.py
+++ linux_funcs.py
@@ -246,7 +246,7 @@ def linux_replica_log(update: Update, _) -> None:
     update.message.reply_text(f'<pre language="python">{log_15_str[495:]}</pre>', parse_mode='HTML')
 
 def linux_replica_log2(update: Update, _) -> None:
-    my_command = "su -c 'cat /tmp/postgresql-my.log'"
+    my_command = 'tail /tmp/postgresql-my.log'
     data = os.system(my_command)
     print(data)
     update.message.reply_text(f'<pre language="python">{data}</pre>', parse_mode='HTML')
